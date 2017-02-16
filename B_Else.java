084121a7941c58e5b4f6c49f3d3978b7e3492486
choyoungkyu <jyk9881@nate.com>
2017-02-16 10:22:00 +0900

    Condition
    조건문

---
+++ B_Else.java
@@ -0,0 +1,14 @@
+package jyk9881.java.Condition;
+
+public class B_Else {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 if (false) {
+	            System.out.println(1);
+	        } else {
+	            System.out.println(2);
+	        }
+	}
+
+}
---
+++ C_Else_if.java
@@ -0,0 +1,18 @@
+package jyk9881.java.Condition;
+
+public class C_Else_if {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 if (false) {
+	            System.out.println(1);
+	        } else if (true) {
+	            System.out.println(2);
+	        } else if (true) {
+	            System.out.println(3);
+	        } else {
+	            System.out.println(4);
+	        }
+	}
+
+}
